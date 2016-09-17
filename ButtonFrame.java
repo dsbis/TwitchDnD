6543b6a328124bae53d7b7ca00917053f64cd3db
JJsugar <lijcf313@hotmail.com>
2016-09-17 14:10:12 -0500

    Add files via upload

--- ButtonFrame.java
+++ ButtonFrame.java
@@ -14,7 +14,6 @@ public class ButtonFrame implements ActionListener{
 	Dice d;
 	JFrame frame;
 	JLabel label;
-	private JTextField textField;
 	
 	public ButtonFrame(Dice dice){
 		this.d = dice;
@@ -25,11 +24,14 @@ public class ButtonFrame implements ActionListener{
 		frame.setSize(frameWidth, frameHeight);
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(192, 373, 69, 20);
+		panel.add(lblNewLabel);
 		JButton button = new JButton("Dice 4");
 		button.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				label.setText(Integer.toString(d.rollDice(4)));
+				lblNewLabel.setText(Integer.toString(d.rollDice(4)));
 			}
 		});
 		button.setBounds(26, 40, 121, 29);
@@ -41,6 +43,12 @@ public class ButtonFrame implements ActionListener{
 		
 		button.addActionListener(this);
 		JButton buttonA = new JButton("Dice 6");
+		buttonA.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				lblNewLabel.setText(Integer.toString(d.rollDice(6)));
+			}
+		});
 		buttonA.setBounds(291, 40, 108, 29);
 		buttonA.setActionCommand("6");
 		buttonA.setActionCommand("8");
@@ -55,6 +63,12 @@ public class ButtonFrame implements ActionListener{
 		//ActionListener listenerA = new clickListener();
 		buttonA.addActionListener(this);
 		JButton buttonB = new JButton("Dice 8");
+		buttonB.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				lblNewLabel.setText(Integer.toString(d.rollDice(8)));
+			}
+		});
 		buttonB.setBounds(26, 108, 121, 29);
 		panel.add(buttonB);
 		
@@ -70,26 +84,41 @@ public class ButtonFrame implements ActionListener{
 		//ActionListener listenerF = new clickListener();
 		buttonB.addActionListener(this);
 		JButton buttonC = new JButton("Dice 10");
+		buttonC.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				lblNewLabel.setText(Integer.toString(d.rollDice(10)));
+			}
+		});
 		buttonC.setBounds(291, 108, 108, 29);
 		panel.add(buttonC);
 		JButton buttonD = new JButton("Dice 12");
+		buttonD.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				lblNewLabel.setText(Integer.toString(d.rollDice(12)));
+			}
+		});
 		buttonD.setBounds(26, 181, 121, 29);
 		panel.add(buttonD);
 		JButton buttonE = new JButton("Dice 20");
-		buttonE.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				label.setText(Integer.toString(d.rollDice(4)));
+		buttonE.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				lblNewLabel.setText(Integer.toString(d.rollDice(20)));
 			}
 		});
+		
 		buttonE.setBounds(293, 181, 106, 29);
 		panel.add(buttonE);
 		JButton buttonF = new JButton("Dice 100");
-		buttonF.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				label.setText(Integer.toString(d.rollDice(4)));
-				
+		buttonF.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				lblNewLabel.setText(Integer.toString(d.rollDice(4)));
 			}
 		});
+		
 		buttonF.setBounds(166, 272, 108, 29);
 		panel.add(buttonF);
 		frame.getContentPane().add(panel);
@@ -97,12 +126,7 @@ public class ButtonFrame implements ActionListener{
 		label.setBounds(0, 0, 0, 0);
 		panel.add(label);
 		
-		textField = new JTextField();
-		textField.setForeground(Color.RED);
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		textField.setBounds(112, 352, 214, 56);
-		panel.add(textField);
-		textField.setColumns(10);
+		
 	}
 
 	/* (non-Javadoc)
@@ -110,26 +134,35 @@ public class ButtonFrame implements ActionListener{
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		switch(e.getActionCommand()){
-		case "4": 
-		label.setText(Integer.toString(d.rollDice(4)));
-		case "6": ;
-		label.setText(Integer.toString(d.rollDice(6)));
-		case "8": ;
-		label.setText(Integer.toString(d.rollDice(8)));
-		case "10": ;
-		label.setText(Integer.toString(d.rollDice(10)));
-		case "12": ;
-		label.setText(Integer.toString(d.rollDice(12)));
-		case "20": ;
-		label.setText(Integer.toString(d.rollDice(20)));
-		case "100": ;
-		label.setText(Integer.toString(d.rollDice(100)));
-		}
+		// TODO Auto-generated method stub
 		
 	}
-}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+
+//	public void actionPerformed(ActionEvent e) {
+//		
+//		switch(e.getActionCommand()){
+//		case "4": 
+//		lblNewLabel.setText(Integer.toString(d.rollDice(4)));
+//		case "6": ;
+//		label.setText(Integer.toString(d.rollDice(6)));
+//		case "8": ;
+//		label.setText(Integer.toString(d.rollDice(8)));
+//		case "10": ;
+//		label.setText(Integer.toString(d.rollDice(10)));
+//		case "12": ;
+//		label.setText(Integer.toString(d.rollDice(12)));
+//		case "20": ;
+//		label.setText(Integer.toString(d.rollDice(20)));
+//		case "100": ;
+//		label.setText(Integer.toString(d.rollDice(100)));
+//		}
+//		
+	}
+
 //	private void createComponents(){
 //		
 //}
